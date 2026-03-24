712f6d6c8eb98d377b275d5d1e1f951fa9ff4216
Chrystopher Thomas <140456118+ScribeE-coder@users.noreply.github.com>
2026-03-23 21:31:13 -0700

    working on strStr

--- # All LeetCode exercises completed, God .py
+++ # All LeetCode exercises completed, God .py
@@ -7,10 +7,11 @@ def concatenation(nums: list[int]):
         nums2.append(num)
     listy = nums + nums2 
     return listy 
-
+""" 
 print(concatenation([1, 2, 3])) 
 print(concatenation([1, 3, 2, 1])) 
 "Beats 100% runtime, 68% memory, I wonder how different data structures would contribute to memory usage"
+"""
 
 
 """Q2: Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn]
@@ -23,11 +24,12 @@ def shuffle(nums:list[int], n:int):
         listy.append(xlist[i])
         listy.append(ylist[i]) 
     return listy 
-
+""" 
 print(shuffle([2,5,1,3,4,7], 3))
 print(shuffle([1,2,3,4,4,3,2,1], 4))
 print(shuffle([1, 1, 2, 2], 2)) 
 "beats 81% runtime wise and 40% memory wise, I think runtime is O(N) how would you reduce that?"
+"""
 
 def maxConsecutiveOnes(listy: list[int]): 
     map = {}
@@ -35,8 +37,44 @@ def maxConsecutiveOnes(listy: list[int]):
         if num in map.keys(): 
             map[num] += 1 
         map[num] = 0 
-
+""" 
 print(maxConsecutiveOnes([1, 1, 0, 1, 1, 1]))
+"""
+
+def find_the_difference(s: str, t: str):
+    s_map = {}
+    t_map = {}
+    if s == "": 
+        return t 
+    
+    for char in s: 
+       s_map[char] = s_map.get(char, 0) + 1 # creating a key with a value and then incrementing by 1 if that key already exists in the dictionary
+
+    for char in t: 
+        t_map[char] = t_map.get(char, 0) + 1 
+
+    for char in t_map.keys(): 
+        if char not in s_map.keys() or t_map[char] != s_map[char]: 
+            return char
+""" 
+print(find_the_difference("abcd", "abcde"))
+print(find_the_difference("", "y")) 
+print(find_the_difference("a", "aa"))
+print(find_the_difference("abbcd", "acdbbe"))
+"""
+
+def strStr(haystack: str, needle: str): 
+    stringy = ""
+    for i in range(len(haystack)): 
+        stringy += haystack[i] 
+        if stringy == needle: 
+            return len(stringy) - (i + 1)
+    return -1 
+
+print(strStr("sadbutsad", "sad"))
+print(strStr("leetcode", "leeto"))
+print(strStr("foodiebyfoot", "by"))
+
 
 
 
